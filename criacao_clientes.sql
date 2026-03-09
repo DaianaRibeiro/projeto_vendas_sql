c49156fcf2bc7dde2dbc7b05956954026f1987d8
Daiana Ribeiro <daiana.arruda.ribeiro@gmail.com>
2026-03-09 18:20:36 -0300

    feat: adiciona coluna data_cadastro na tabela clientes
    feat: adiciona coluna data_cadastro na tabela clientes

--- criacao_clientes.sql
+++ criacao_clientes.sql
@@ -2,5 +2,6 @@ CREATE TABLE clientes (
     id_cliente AUTO_INCREMENT PRIMARY KEY,
     nome_cliente VARCHAR(100) NOT NULL,
     email_cliente VARCHAR(100) NOT NULL,
-    telefone_cliente VARCHAR(20) NOT NULL
+    telefone_cliente VARCHAR(20) NOT NULL,
+    data_cadastro DATE
 );
