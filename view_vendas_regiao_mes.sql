84e0452ada66ff20facab499dac118797c612b4c
Daiana Ribeiro <daiana.arruda.ribeiro@gmail.com>
2026-03-09 18:24:04 -0300

    criação da view de vendas agregadas por região e mês
    criação da view de vendas agregadas por região e mês

---
+++ view_vendas_regiao_mes.sql
@@ -0,0 +1,16 @@
+CREATE VIEW view_vendas_regiao_mes AS
+SELECT
+    c.regiao,
+    strftime('%Y', v.data_venda) AS ano_venda,
+    strftime('%m', v.data_venda) AS mes_venda,
+    SUM(v.valor_venda) AS total_vendido,
+    AVG(v.valor_venda) AS ticket_medio,
+    COUNT(v.id_venda) AS total_pedidos
+FROM vendas v
+JOIN clientes c
+ON v.id_cliente = c.id_cliente
+WHERE strftime('%Y', v.data_venda) = '2026'
+GROUP BY
+    c.regiao,
+    ano_venda,
+    mes_venda;
