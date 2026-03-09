bc45cc30f6121bf2e049aca5b8f38942a37c8f40
Daiana Ribeiro <daiana.arruda.ribeiro@gmail.com>
2026-03-09 18:19:13 -0300

    Create clientes table for client data storage
    commit inicial: criação da tabela clientes

---
+++ criacao_clientes.sql
@@ -0,0 +1,6 @@
+CREATE TABLE clientes (
+    id_cliente AUTO_INCREMENT PRIMARY KEY,
+    nome_cliente VARCHAR(100) NOT NULL,
+    email_cliente VARCHAR(100) NOT NULL,
+    telefone_cliente VARCHAR(20) NOT NULL
+);
